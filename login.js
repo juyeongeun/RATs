e2fe836415cb2375ce6e1a19722a4badd15d82e8
jjuyeongeun <80696592+juyeongeun@users.noreply.github.com>
2025-03-07 14:37:48 +0900

    Update login.js

--- login.js
+++ login.js
@@ -12,11 +12,11 @@ const port = 8080;
 
 //mysql 정보
 const connection = mysql.createConnection({
-    host: "127.0.0.1",
-    user: "root",
-    password: "Ju981203@",
-    database: "w_info",
-    port:"3306"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 //mysql 연결
